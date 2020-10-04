34486136a7b2460033686d67b97dc08377300986
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2020-10-04 09:53:18 -0400

    feat: Add implementations for every member function

--- src/connection.cc
+++ src/connection.cc
@@ -24,28 +24,102 @@
 
 #include "connection.h"
 
-Connection::Connection()
+Connection::Connection() :
+	generic_database_(nullptr),
+	type_database_("Unknown"),
+	server_address_("127.0.0.1"),
+	status_("Not connected."),
+	name_database_("Unknown"),
+	user_("Unknown"),
+	password_("")
 {
-	status_ = "Not connected";
 }
 
 Connection::~Connection()
 {
 	
 }
+std::string Connection::get_type_database() const
+{
+	return type_database_;
+}
+std::string Connection::get_server_address() const
+{
+	return server_address_;
+}
 
+std::string Connection::get_status() const
+{
+	return status_;
+}
+
+std::string Connection::get_name_database() const
+{
+	return name_database_;
+}
+
+std::string Connection::get_port_connection() const
+{
+	return port_connection_;
+}
+
+std::string Connection::get_user() const
+{
+	return user_;
+}
+
+std::string Connection::get_password() const
+{
+	return password_;
+}
+
+void Connection::set_type_database(std::string type_database)
+{
+	type_database_ = type_database;
+}
+
+void Connection::set_server_address(std::string server_address)
+{
+	server_address_ = server_address;
+}
+
+void Connection::set_status(std::string status)
+{
+	status_ = status;
+}
+
+void Connection::set_name_database(std::string name_database)
+{
+	name_database_ = name_database;
+}
+
+void Connection::set_port_connection(std::string port_connection)
+{
+	port_connection_ = port_connection;
+}
+
+void Connection::set_user(std::string user)
+{
+	user_ = user;
+}
+
+void Connection::set_password(std::string password)
+{
+	password_ = password;
+}
+		
 bool Connection::Init_()
 {
-	if(type_database == "sqlite3"))
+	if(type_database_ == "sqlite3")
 	{
-		generic_database_ = new SQLite3Handler();
+		generic_database_ = new SQLite3Handler;
 		status_ = "Connection init.";
 		return true;
 	}
-	else if(type_database == "mysql"))
+	else if(type_database_ == "mysql")
 	{
-		generic_database_ = new MySQLHandler();
-		status_ = "Connection init."
+		generic_database_ = new MySQLHandler;
+		status_ = "Connection init.";
 		return true;
 	}
 	else
@@ -55,8 +129,31 @@ bool Connection::Init_()
 	}
 }
 
-void Connection::End_()
+bool Connection::Restart_()
 {
-	status_ = "Connection closed.";
-	delete generic_database_;
+	if(generic_database_ == nullptr)
+	{
+		status_ = "Not connection found.";
+		return false;
+	}
+	else
+	{
+		delete generic_database_;
+		return Init_();
+	}
+}
+
+bool Connection::End_()
+{
+	if(generic_database_ == nullptr)
+	{
+		status_ = "Not connection found.";
+		return false;
+	}
+	else
+	{
+		status_ = "Connection closed.";
+		delete generic_database_;
+		return true;
+	}
 }
