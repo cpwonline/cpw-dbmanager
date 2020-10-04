911822daa3b99bca7846d5f70b74b3f2d299321b
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2020-10-04 09:54:02 -0400

    feat: Add every implementation for member functions

--- src/cpw_dbmanager.cc
+++ src/cpw_dbmanager.cc
@@ -27,35 +27,69 @@
 
 CPWDBManager::CPWDBManager()
 {
-	
+	connections_handler_ = new std::vector<Connection*>;
 }
 
-void CPWDBManager::NewConnection_(string type_connection, string name_database, string port_connection, string user, string password)
+CPWDBManager::~CPWDBManager()
 {
-	
+	for(int a = 0; a < connections_handler_->size(); a++) delete (*connections_handler_)[a];
+	delete connections_handler_;
+}
+
+void CPWDBManager::NewConnection_(std::string type_database)
+{
+	connections_handler_->push_back(new Connection());
+	auto last_connection = connections_handler_->back();
+	last_connection->set_type_database(type_database);
+}
+
+void CPWDBManager::ServerConfigs_(std::string server_address, std::string port_connection)
+{
+	auto last_connection = connections_handler_->back();
+	last_connection->set_server_address(server_address);
+	last_connection->set_port_connection(port_connection);
+}
+void CPWDBManager::DatabaseConfigs_(std::string name_database, std::string user, std::string password)
+{
+	auto last_connection = connections_handler_->back();
+	last_connection->set_name_database(name_database);
+	last_connection->set_user(user);
+	last_connection->set_password(password);
 }
 
 void CPWDBManager::DeleteConnection_(int number_connection)
 {
-	
+	//connections_handler_->erase(number_connection);
 }
 
 void CPWDBManager::DeleteLastConnection_()
 {
-	
+	//connections_handler_->erase(connections_handler_->end());
 }
 
 Connection* CPWDBManager::ViewConnection_(int number_connection)
 {
-	
+	Connection* connection_requested = connections_handler_->at(number_connection);
+}
+
+void CPWDBManager::ShowConnections_()
+{
+	int c = 0;
+	for(auto& a : connections_handler_)
+	{
+		std::cout << "\n" << c++ << ". " << a->name_database_ << ", " << a->get_status();
+	}
 }
 
 bool CPWDBManager::ConnectionEmpty_()
 {
-	
+	if(connections_handler_->empty())
+		return true;
+	else
+		return false;
 }
 
 int CPWDBManager::TotalConnections_()
 {
-	
+	return connections_handler_->size();
 }
