0069f35ccb60b8960d48fc8ed1f3d60047d4f828
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-02-22 18:18:33 +0000

    feat: Add classes of the application structure
    - This classes implements every parte of the software

---
+++ include/row.h
@@ -0,0 +1,38 @@
+/*
+ * row.h
+ * 
+ * Copyright 2021 Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#ifndef ROW_H
+#define ROW_H
+
+class Row
+{
+	public:
+		Row();
+		virtual ~Row();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* ROW_H */ 
