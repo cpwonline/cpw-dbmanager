cbcbe49770f547389b58dafc73e5529030db9b64
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-03-09 13:15:30 +0000

    doc: Change the print messages structure on tests

--- tests/test_gen.cc
+++ tests/test_gen.cc
@@ -28,27 +28,27 @@ void TestGen::TearDown()
 
 TEST_F(TestGen, ViewData)
 {
-	std::cout << "\n-- ViewData";
+	std::cout << "\n- ViewData";
 	
 	TestObj_->CreateConnection_(CPWDBManager::TypeDB::MariaDB, "localhost", "3033", "test", "root", "26552160jfrc");
 	
 	int a = 0;
 	for(auto it = TestObj_->get_connections_collector()->begin(); it != TestObj_->get_connections_collector()->end(); ++it)
 	{
-		std::cout << "\n" << ++a
-			<< "\n- Database: " << (*it)->get_connected_database()->get_name()
-			<< "\n- Access data: " << (*it)->get_current_access_data()->get_username() << ", " << (*it)->get_current_access_data()->get_password()
-			<< "\n- Address: " << (*it)->get_current_address()->get_internet_address() << ", " << (*it)->get_current_address()->get_port()
+		std::cout << "\n-- " << ++a
+			<< "\n--- Database: " << (*it)->get_connected_database()->get_name()
+			<< "\n--- Access data: " << (*it)->get_current_access_data()->get_username() << ", " << (*it)->get_current_access_data()->get_password()
+			<< "\n--- Address: " << (*it)->get_current_address()->get_internet_address() << ", " << (*it)->get_current_address()->get_port()
 		;
 	}
-	std::cout << "\n--\n";
+	std::cout << "\n-\n";
 	
 	ASSERT_GT(a, 0);
 }
 
 TEST_F(TestGen, InitDatabase)
 {
-	std::cout << "\n-- InitDatabase";
+	std::cout << "\n- InitDatabase";
 	
 	TestObj_->CreateConnection_(CPWDBManager::TypeDB::MariaDB, "localhost", "3033", "test", "root", "26552160jfrc");
 	auto it = TestObj_->get_connections_collector()->back(); 
@@ -58,17 +58,17 @@ TEST_F(TestGen, InitDatabase)
 		ASSERT_TRUE(it->get_connected_database()->get_state());
 		if(it->get_connected_database()->get_state())
 		{
-			std::cout << "\nConectado";
+			std::cout << "\n-- Conectado";
 		}
 		it->get_connected_database()->Disconnect_();
 	}
 	
-	std::cout << "\n--\n";
+	std::cout << "\n-\n";
 }
 
 TEST_F(TestGen, TestQuery)
 {
-	std::cout << "\n-- TestQuery";
+	std::cout << "\n- TestQuery";
 	
 	// Create the connection
 	TestObj_->CreateConnection_(CPWDBManager::TypeDB::MariaDB, "localhost", "3033", "test", "root", "26552160jfrc");
@@ -94,30 +94,33 @@ TEST_F(TestGen, TestQuery)
 			it->NewQuery_("DROP DATABASE test4;");
 			
 			// View states and results of queries
+			int a = 0;
 			for(auto it2 = it->get_queries()->begin(); it2 != it->get_queries()->end(); ++it2)
 			{
+				std::cout << "\n-- " << ++a;
 				if((*it2)->get_state())
 				{
-					std::cout << "\n -- Consulta: " << (*it2)->get_query();
-					std::cout << "\n -- Veces usada: " << (*it2)->get_times_used();
+					std::cout << "\n--- Consulta: " << (*it2)->get_query();
+					std::cout << "\n--- Veces usada: " << (*it2)->get_times_used();
 				}
 				else
-					std::cout << "\n -- Error de consulta: " << (*it2)->get_error();
+					std::cout << "\n--- Error de consulta: " << (*it2)->get_error();
+				a++;
 			}
 		}
 		else
-			std::cout << "\n -- Error en la base: " << it->get_connected_database()->get_error();
+			std::cout << "\n-- Error en la base: " << it->get_connected_database()->get_error();
 			
 		// Disconnect from the database
 		it->get_connected_database()->Disconnect_();
 	}
 	
-	std::cout << "\n--\n";
+	std::cout << "\n-\n";
 }
 
 TEST_F(TestGen, ViewResults)
 {
-	std::cout << "\n-- ViewResults";
+	std::cout << "\n- ViewResults";
 	
 	// Create the connection
 	TestObj_->CreateConnection_(CPWDBManager::TypeDB::MariaDB, "localhost", "3033", "test", "root", "26552160jfrc");
@@ -138,26 +141,29 @@ TEST_F(TestGen, ViewResults)
 			it->NewQuery_("SELECT * FROM carro;", true);
 			
 			// View states and results of queries
+			int a = 0;
 			for(auto it2 = it->get_queries_results()->begin(); it2 != it->get_queries_results()->end(); ++it2)
 			{
+				std::cout << "\n-- " << ++a;
 				if(it2->first->get_state())
 				{
-					std::cout << "\n -- Consulta: " << it2->first->get_query();
-					std::cout << "\n -- Veces usada: " << it2->first->get_times_used();
-					std::cout << "\n -- Resultados: ";
+					std::cout << "\n--- Consulta: " << it2->first->get_query();
+					std::cout << "\n--- Veces usada: " << it2->first->get_times_used();
+					std::cout << "\n--- Resultados: ";
 					
 					auto results = it2->second->get_results_table()->get_columns();
-					int a = 0;
+					int b = 0;
 					for(auto col = results->begin(); col != results->end(); ++col)
 					{
-						std::cout << "\n" << a << ". Columna";
+						std::cout << "\n---- " << b << ". Columna";
 						for(auto value = (*col)->get_row()->get_values()->begin(); value != (*col)->get_row()->get_values()->end(); ++value)
-							std::cout << "\n---- Key: " << value->first << ", value: " << value->second;
-						a++;
+							std::cout << "\n----- Key: " << value->first << ", value: " << value->second;
+						b++;
 					}
 				}
 				else
-					std::cout << "\n -- Error de consulta: " << it2->first->get_error();
+					std::cout << "\n --- Error de consulta: " << it2->first->get_error();
+				a++;
 			}
 		}
 		else
@@ -167,7 +173,7 @@ TEST_F(TestGen, ViewResults)
 		it->get_connected_database()->Disconnect_();
 	}
 	
-	std::cout << "\n--\n";
+	std::cout << "\n-\n";
 }
 
 //-----------------------------------------------------------------------------
