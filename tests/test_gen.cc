9135e71c557ce4efb8e67ced62852b6a6c5360a4
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-03-11 19:30:36 +0000

    test: Change the names of the targets objects

--- tests/test_gen.cc
+++ tests/test_gen.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "gtest/gtest.h"
-#include "Biblioteca.h"
+#include "library2.h"
 
 class TestGen : public ::testing::Test
 {
@@ -8,47 +8,33 @@ class TestGen : public ::testing::Test
 		void SetUp() override;
 		void TearDown() override;
 
-		Biblioteca *TestObj_;
+		Library2 *TestObj2_;
 };
 
 //-----------------------------------------------------------------------------
 
 void TestGen::SetUp()
 {
-    TestObj_ = new Biblioteca();
+	TestObj2_ = new Library2();
 }
 
 void TestGen::TearDown()
 {
-    delete TestObj_;
+	delete TestObj2_;
 }
 
 //-----------------------------------------------------------------------------
 
-TEST_F(TestGen, Add)
+TEST_F(TestGen, TestLib2)
 {
-    ASSERT_EQ(5, TestObj_->add(3, 2));
-    ASSERT_EQ(8, TestObj_->add(4, 4));
-    ASSERT_EQ(12, TestObj_->add(9, 3));
+	ASSERT_EQ("Hello!", TestObj2_->get_var1());
 }
 
-TEST_F(TestGen, Sub)
-{
-    ASSERT_EQ(4, TestObj_->sub(6, 2));
-    ASSERT_EQ(10, TestObj_->sub(15, 5));
-}
-
-TEST_F(TestGen, Mul)
-{
-    ASSERT_EQ(6, TestObj_->mul(3, 2));
-    ASSERT_EQ(27, TestObj_->mul(3, 9));
-    ASSERT_EQ(12, TestObj_->mul(2, 6));
-}
 
 //-----------------------------------------------------------------------------
 
 int main(int argc, char* argv[])
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
