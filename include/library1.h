885662420ef64163c9136b4bed44eb9a2010308f
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-03-13 14:11:51 +0000

    doc: Edit the licence notices
    - Update to version 3 of GNU GPL
    - Change name of developer and email

--- include/library1.h
+++ include/library1.h
@@ -1,11 +1,11 @@
 /*
  * library1.h
  * 
- * Copyright 2021 Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
+ * Copyright 2021 CPW Online <support@cpwonline.org>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
